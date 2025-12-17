9ffe724600024e3ebd8c0696e984161108689ad2
Shivam <shivam@Shivams-MacBook-Air-3.local>
2025-12-17 14:11:17 +0530

    set up user model

---
+++ src/models/video.model.js
@@ -0,0 +1,50 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePageinate from "mongoose-aggregate-paginate-v2"
+
+
+const videoSchema = new Schema ({
+    videFile: {
+        type: String,
+        required: true,
+
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+
+    },
+    title: {
+        type: String,
+        required: true,
+
+    },
+    description: {
+        type: String,
+        required: true,
+
+    },
+    duration: {
+        type: Number,
+        required: true,
+
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    isPUlbished : {
+        type: Boolean,
+        default: true
+
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+}, {timestamps: true})
+
+
+videoSchema.plugin(mongooseAggregatePageinate)
+
+export const Video = mongoose.model("Video", videoSchema)
